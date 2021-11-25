752719f0ff3ef5a9b920a5de52a482d85ce031e8
professorbruh <sidharthrajagopal1123@gmail.com>
2021-11-25 22:51:20 +0530

    made changes to comment repo

--- src/main/java/com/pubtrack/pubtrack/CommentRepo.java
+++ src/main/java/com/pubtrack/pubtrack/CommentRepo.java
@@ -1,14 +1,9 @@
 package com.pubtrack.pubtrack;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-public interface CommentRepo extends CrudRepository<Comment,String>
+public interface CommentRepo extends CrudRepository<Comment, String>
 {
-    @Query(value="select comment.reviewer_id,paper.title,editor.name,comment.comments"+
-    "from comment inner join paper on comment.reference_id=paper.ref_id"+
-    "inner join editor on editor.pub_id=paper.pub_id where comment.reviewer_id= :reviewerID;",
-    nativeQuery=true)
-    List<Comment> findBySearchTermNative(String reviewerID);
+    
 }
